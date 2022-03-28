dc7e974f2cc082101a4bcd827912920056a3e422
manuel.blanc <manuel.blanc@fatshark.se>
2022-03-28 23:04:18 +0200

    My first triangle

--- main.js
+++ main.js
@@ -38,14 +38,43 @@ class Canvas {
 		}
 		this.image = this.context.getImageData(0, 0, this.width, this.height);
 	}
+	line(x0, y0, x1, y1, rgba) { // Bresenham's line algorithm.
+		const dx = Math.abs(x1 - x0);
+		const sx = x0 < x1 ? 1 : -1;
+		const dy = -Math.abs(y1 - y0);
+		const sy = y0 < y1 ? 1 : -1;
+		let error = dx + dy;
+		while (true) {
+			this.set(x0, y0, rgba);
+			if (x0 === x1 && y0 === y1) return;
+			const e2 = 2*error;
+			if (e2 >= dy) {
+				if (x0 === x1) return;
+				error += dy;
+				x0 += sx;
+			}
+			if (e2 <= dy) {
+				if (y0 === y1) return;
+				error += dx;
+				y0 += sy;
+			}
+		}
+	}
 }
 
 const canvas = new Canvas("renderer", 800, 600);
-for (let y = 0; y < canvas.height; ++y) {
-	for (let x = 0; x < canvas.width; ++x) {
-		const u = x/canvas.width*255;
-		const v = y/canvas.height*255;
-		canvas.set(x, y, [ u, v, 255-u, 255 ])
-	}
-}
+canvas.clear([0,0,0,255]);
+const hw = canvas.width*0.5, hh = canvas.height*0.5;
+const dy = 150;
+const dx = Math.floor(dy*Math.sqrt(3/2)); // cos(30)
+const x0 = hw;
+const y0 = hh - dy;
+const x1 = hw - dx;
+const y1 = hh + dy;
+const x2 = hw + dx;
+const y2 = hh + dy;
+const white = [255,255,255,255]
+canvas.line(x0,y0, x1,y1, white);
+canvas.line(x1,y1, x2,y2, white);
+canvas.line(x2,y2, x0,y0, white);
 canvas.present();
